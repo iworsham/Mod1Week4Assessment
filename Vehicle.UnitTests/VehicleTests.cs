64933b4cc1c07945b83a576204397028d19c7d55
iworsham <15iworsham@gmail.com>
2023-04-13 12:25:12 -0400

    work completed 12:24 PM

--- Vehicle.UnitTests/VehicleTests.cs
+++ Vehicle.UnitTests/VehicleTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 namespace Vehicle.UnitTests
 {
     public class VehicleTests
@@ -17,10 +18,30 @@ namespace Vehicle.UnitTests
         public void Summary_ReturnsInfoAboutVehicle()
         {
             Vehicle andysBike = new Vehicle(2, "Blue");
+            andysBike.Summary();
+            Assert.Equal(2, andysBike.NumberOfWheels);
+            Assert.Equal("Blue", andysBike.Color);
+            Assert.Equal(0, andysBike.MilesDriven);
 
             var expected = "This Blue vehicle has 2 wheels and has driven 0 miles";
 
 
+        }
+        [Fact]
+        public void DoesMethod_CalcualateMilesDriven()
+        {
+            Vehicle zaysCar = new Vehicle(4, "Silver");
+            zaysCar.Drive();
+            Assert.Equal(5, zaysCar.MilesDriven);
+        }
+
+        [Fact]
+        public void DoesMethod_ChangeColorOfCar()
+        {
+            Vehicle zayCar = new Vehicle(4, "Silver");
+
+            Assert.Equal("Silver", zayCar.Color);
+            zayCar.Paint("Black");
         }
 
         // Add more tests here!
